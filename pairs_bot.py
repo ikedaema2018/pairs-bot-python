a738b824b2f35f9414371b1f400068ff02a5a421
ikedaema2018 <ikedaema0817@gmail.com>
2019-02-28 01:51:07 +0900

    自分のコミュに入ってる人をクリックする処理完了？

--- pairs_bot.py
+++ pairs_bot.py
@@ -121,24 +121,41 @@ class PairsComu():
             time.sleep(random.randint(4, 8))
 
         def joined_comu_member_click(self):
-            joined_comu_members = self.driver.find_elements(By.XPATH, "//ul[@class='list_view_users']/li")
-            what_time_click_joined_comu_member = random.randint(0, len(joined_comu_members))
-            range_from_joined_comu_members = list(range(len(joined_comu_members)))
 
-            for _ in range(what_time_click_joined_comu_member):
-                click_member = random.randint(0, len(range_from_joined_comu_members))
-                joined_comu_members[click_member].find_element_by_tag_name("a").click()
-                time.sleep(random.randint(0, 3))
-                
+            for _ in range(random.randint(3, 10)):
 
+                time.sleep(random.randint(4, 8))
+                joined_comu_members = self.driver.find_elements(By.XPATH, "//ul[@class='list_view_users']/li")
+                what_time_click_joined_comu_member = random.randint(0, len(joined_comu_members) - 1)
 
+                for _ in range(what_time_click_joined_comu_member):
+                    print(len(joined_comu_members))
+                    time.sleep(random.randint(3, 7))
+                    click_member = random.randint(0, len(joined_comu_members) - 1)
+                    self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    joined_comu_members.pop(click_member).find_element_by_tag_name("a").click()
+                    # TODO あとで0~20秒に
+                    time.sleep(random.randint(2, 7))
+                    self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    self.driver.find_element_by_class_name("box_modal_personal_view").find_element_by_tag_name("a").click()
+                    time.sleep(random.randint(2, 7))
+
+                time.sleep(random.randint(0, 5))
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                self.driver.find_element_by_xpath(
+                    "//a[@class='pager_next pager_item button_c button_white_a']").click()
 
         def see_my_comu_main(self):
-            # TODO 2列目のゲームナンバーに行く処理も書く
-            game_number = random.randint(0, 4)
-            self.joined_comu_enter(game_number)
-            time.sleep(random.randint(3, 6))
-            self.joined_comu_member_click()
+            try:
+                # TODO 2列目のゲームナンバーに行く処理も書く
+                game_number = random.randint(0, 4)
+                self.joined_comu_enter(game_number)
+                time.sleep(random.randint(3, 6))
+                self.joined_comu_member_click()
+            except Exception as e:
+                print(e, 'error occurred')
+                time.sleep(30)
+
 
 
     def pairs_comu_main(self):
@@ -149,7 +166,7 @@ class PairsComu():
         see_comu = self.SeeMyComu(self.driver)
         see_comu.see_my_comu_main()
 
-# TODO全てのサブモジュールの共通関数を支配するクラスを作成
+# TODO 全てのサブモジュールの共通関数を支配するクラスを作成
 
 
 
